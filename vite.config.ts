14494d1a47f6290b0d56c1d60c24948e3bb509b6
Sean OBrien <sobrien@gmail.com>
2026-01-18 14:31:08 -0500

    Removed build path configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import devtoolsJson from "vite-plugin-devtools-json";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/my-stuff/" : "/",
   plugins: [reactRouter(), tsconfigPaths(), devtoolsJson()],
 }));
