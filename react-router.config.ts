d24ece6e2850f20c37efc708777166dce3f2d853
Sean OBrien <sobrien@gmail.com>
2026-01-18 00:00:42 -0500

    Test config

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,5 +1,3 @@
 import type { Config } from "@react-router/dev/config";
 
-export default {
-  ssr: false,
-} satisfies Config;
+export default { basename: "/my-stuff", ssr: false } satisfies Config;
