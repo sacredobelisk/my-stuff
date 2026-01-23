14ee8ae53e80e6c607bb8484fdf4336c6b6097ac
Sean OBrien <sobrien@gmail.com>
2026-01-23 00:04:58 -0500

    Fix broken build

--- app/components/bill-calculator/use-bill-calculator-people.tsx
+++ app/components/bill-calculator/use-bill-calculator-people.tsx
@@ -1,14 +1,15 @@
 import { useCallback, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import { useLocalStorage } from "~/hooks/useLocalStorage";
 import type { BillData, Person } from "./types";
-import { createDefaultBillData, generateKey } from "./utils";
+import { createDefaultBillData } from "./utils";
 
 export function useBillCalculatorPeople() {
   const [savedData] = useLocalStorage<BillData>("billCalculator", createDefaultBillData());
 
   const [people, setPeople] = useState<Person[]>(savedData.people);
 
-  const addPerson = () => setPeople((prevPeople) => [...prevPeople, { key: generateKey(), name: "", subtotal: 0 }]);
+  const addPerson = () => setPeople((prevPeople) => [...prevPeople, { key: uuidv4(), name: "", subtotal: 0 }]);
 
   const removePerson = useCallback((key: string) => {
     setPeople((prevPeople) => {
